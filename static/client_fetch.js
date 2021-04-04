f712784d8fd796d9881c535281895b83b070ee1a
Neil T <neiltz711@gmail.com>
2021-04-04 01:19:10 -0400

    Added LOGOUt button

--- static/client_fetch.js
+++ static/client_fetch.js
@@ -108,3 +108,17 @@ function fetchTickets() {
 }
 
 fetchTickets();
+
+// For logout button
+function deleteAllCookies() {
+  var cookies = document.cookie.split(';');
+
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i];
+    var eqPos = cookie.indexOf('=');
+    var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+  }
+
+  window.location.replace('http://localhost:7000/login');
+}
--- static/fetch.js
+++ static/fetch.js
@@ -1,7 +1,7 @@
 // If user is a client then we redirect to client dashboard
 if (
-  document.cookie.split(';')[0] == 'client=1' ||
-  document.cookie.split(';')[1] == 'client=1'
+  document.cookie.split(';')[0].trim() == 'client=1' ||
+  document.cookie.split(';')[1].trim() == 'client=1'
 ) {
   window.location.replace('http://localhost:7000/client_dashboard');
 }
@@ -66,4 +66,18 @@ if (
   document.cookie.split(';')[1] == 'client=0'
 ) {
   fetchTickets();
+}
+
+// For logout button
+function deleteAllCookies() {
+  var cookies = document.cookie.split(';');
+
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i];
+    var eqPos = cookie.indexOf('=');
+    var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+  }
+
+  window.location.replace('http://localhost:7000/login');
 }
